3a3ab30fb41c79ff4889cad54673c9b1b63ff35f
Rob Saunders <rob@wearebrandnew.com>
2020-01-11 08:33:43 +0800

    correctly shows model code in ace editor

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
-var JsonRpcWs = require('json-rpc-ws');
-var reader = import('./reader');
+var JsonRpcWs = require("json-rpc-ws");
+// var reader = import('./reader');
 var server = JsonRpcWs.createServer();
 
-server.expose('list-models', function mirror (params, reply) {
-    console.log('list-models called. params: ', params);
-    reply(null, [{"name": "Pet"}]);
+server.expose("list-models", function mirror(params, reply) {
+  console.log("list-models called. params: ", params);
+  reply(null, [{ name: "Pet", code: "function pet() {}" }]);
 });
 
-server.start({ port: 8080 }, function started () {
-    console.log('Server started on port 8080');
+server.start({ port: 8080 }, function started() {
+  console.log("Server started on port 8080");
 });
