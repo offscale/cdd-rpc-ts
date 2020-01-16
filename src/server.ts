8dc98aa8af8aefe5d53896b55dc1ed62732a7a5a
Rob Saunders <rob@wearebrandnew.com>
2020-01-16 14:35:21 +0800

    stub code for generateCode

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,21 @@ var JsonRpcWs = require("json-rpc-ws");
 // var reader = import('./reader');
 var server = JsonRpcWs.createServer();
 
-server.expose("list-models", function mirror(params, reply) {
-  console.log("list-models called. params: ", params);
-  reply(null, [{ name: "Pet", code: "function pet() {}" }]);
+server.expose("generateCode", function mirror(params, reply) {
+  console.log("generateCode -> params: ", params);
+  reply(null, { code: "function pet() {}" });
 });
 
-server.start({ port: 8080 }, function started() {
-  console.log("Server started on port 8080");
+server.expose("parse", function mirror(params, reply) {
+  console.log("parse -> params: ", params);
+  reply(null, { models: ["Doggo"] });
+});
+
+// server.expose("list-models", function mirror(params, reply) {
+//   console.log("list-models called. params: ", params);
+//   reply(null, [{ name: "Pet", code: "function pet() {}" }]);
+// });
+
+server.start({ port: 7778 }, function started() {
+  console.log("Server started on port 7778");
 });
